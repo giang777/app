7e126f7dfc738afcc7473e7af0cc557eff759fac
giang777 <cucosieu@gmail.com>
2023-05-12 14:32:55 +0700

    up

--- app/src/main/java/com/example/appagile/MainActivity.java
+++ app/src/main/java/com/example/appagile/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     //Xin chào ae :))
+    //XIn chào 2
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
