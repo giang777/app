89584b6df2cdf9240d9c303bbbe764f6a03725be
dell <quyddph26439@fpt.edu.vn>
2023-06-02 17:13:28 +0700

    hoan thien login check remember va doi mat khau

--- app/src/main/java/com/example/appagile/activity/Login.java
+++ app/src/main/java/com/example/appagile/activity/Login.java
@@ -3,9 +3,11 @@ package com.example.appagile.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,6 +19,7 @@ public class Login extends AppCompatActivity {
     private EditText editText1,editText2;
     private Button button;
     private ThuThuDao dao;
+    private CheckBox chkrem;
     @Override
     //tk: admin ( hoa hay thường đều đc )
     //mk : 0343729937
@@ -24,6 +27,17 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         anh_xa();
+
+        SharedPreferences pref = getSharedPreferences("USER_FILE",MODE_PRIVATE);
+        String user = pref.getString("USERNAME","");
+        String pass = pref.getString("PASSWORD","");
+        Boolean rem = pref.getBoolean("REMEMBER",false);
+
+        editText1.setText(user);
+        editText2.setText(pass);
+        chkrem.setSelected(rem);
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,20 +48,35 @@ public class Login extends AppCompatActivity {
 
                 if(ckeck == 1){
                     Toast.makeText(Login.this, "Thành công", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(Login.this,MainActivity.class);
-                    ThuThu thuThu = dao.getID(tk);
-                    intent.putExtra("thuthuLogin",thuThu);
-                    startActivity(intent);
+                    rememberUser(tk,mk,chkrem.isChecked());
+                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    i.putExtra("user",tk);
+                    startActivity(i);
+                    finish();
                 }else{
                     Toast.makeText(Login.this, "Thất bại", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+    public void rememberUser(String u,String p,boolean status){
+        SharedPreferences pref = getSharedPreferences("USER_FILE",MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        if(!status){
+            edit.clear();
+        }else{
+            edit.putString("USERNAME",u);
+            edit.putString("PASSWORD",p);
+            edit.putBoolean("REMEMBER",status);
+        }
+        edit.commit();
+    }
+
 
     private void anh_xa(){
         editText1 = findViewById(R.id.avtivity_login_TK);
         editText2 = findViewById(R.id.avtivity_login_MK);
+        chkrem = findViewById(R.id.avtivity_login_CKB);
         button = findViewById(R.id.avtivity_login_BTN);
         dao = new ThuThuDao(this);
     }
--- app/src/main/java/com/example/appagile/fragment/tai_khoan/DoiMatKhau.java
+++ app/src/main/java/com/example/appagile/fragment/tai_khoan/DoiMatKhau.java
@@ -1,20 +1,29 @@
 package com.example.appagile.fragment.tai_khoan;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.appagile.R;
+import com.example.appagile.dao.ThuThuDao;
+import com.example.appagile.elements.ThuThu;
 
 
 public class DoiMatKhau extends Fragment {
+
+    private EditText oldpass,newpass,renewpass;
+    private Button btnUpdate;
+    private ThuThu thuThu;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,11 +34,38 @@ public class DoiMatKhau extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_doi_mat_khau,container,false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.oldpass = view.findViewById(R.id.FRAGMENT_CHANGE_PASSWORD_ETEXT_OLDPASS);
+        this.newpass = view.findViewById(R.id.FRAGMENT_CHANGE_PASSWORD_ETEXT_NEWPASS);
+        this.renewpass = view.findViewById(R.id.FRAGMENT_CHANGE_PASSWORD_ETEXT_NEWPASS2);
+        this.btnUpdate = view.findViewById(R.id.FRAGMENT_CHANGE_PASSWORD_BTN_CHANGE);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                validate();
+
+                SharedPreferences pref = getActivity().getSharedPreferences("USER_FILE", Context.MODE_PRIVATE);
+                String user = pref.getString("USERNAME","");
+
+
+                ThuThuDao thuThuDao = new ThuThuDao(getActivity());
+                thuThu = thuThuDao.getID(user);
+                thuThu.setMkTT(newpass.getText().toString());
+                long check = thuThuDao.update(thuThu);
+                if(check < 1){
+                    Toast.makeText(getActivity(), "Update không thành công", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(getActivity(), "Update thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -41,5 +77,26 @@ public class DoiMatKhau extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+    private int validate(){
+        int check =1;
+        if(oldpass.getText().length() <=0 || newpass.getText().length() <=0 || renewpass.getText().length() <=0) {
+            Toast.makeText(getActivity(), "Không được để chống", Toast.LENGTH_SHORT).show();
+        }else{
+            SharedPreferences pref = getActivity().getSharedPreferences("USER_FILE",Context.MODE_PRIVATE);
+            String passold = pref.getString("PASSWORD","");
+            String pass = newpass.getText().toString();
+            String repass = renewpass.getText().toString();
+
+            if(!passold.equals(oldpass.getText().toString())){
+                Toast.makeText(getActivity(), "Mật khẩu cũ sai", Toast.LENGTH_SHORT).show();
+                check = -1;
+            }
+            if(!pass.equals(repass)){
+                Toast.makeText(getActivity(), "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
+                check =-1;
+            }
+        }
+        return check;
     }
 }
